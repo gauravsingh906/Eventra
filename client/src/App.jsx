6515b4c36750a5b5c9d99e23a582b6258a6c98fd
gauravsingh906 <gauravsingh8xm@gmail.com>
2025-02-08 15:24:28 +0530

    fixed url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ import PaymentSummary from './pages/PaymentSummary';
 import TicketPage from './pages/TicketPage';
 import CreateEvent from './pages/CreateEvent';
 
-axios.defaults.baseURL = 'http://eventra-back.onrender.com/';
+axios.defaults.baseURL = 'https://eventra-back.onrender.com/';
 axios.defaults.withCredentials = true;
 
 function App() {
