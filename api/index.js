123e3c977a945a55b72cce797f5fa77694563ed7
gauravsingh906 <gauravsingh8xm@gmail.com>
2025-02-08 15:44:39 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,13 +17,13 @@ const jwtSecret = "bsbsfbrnsftentwnnwnwn";
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-   cors({
-      credentials: true,
-      origin: "*",
-   })
-);
-
+app.use(function (req, res, next) {
+   //Enabling CORS
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+     next();
+   });
 // ✅ Cloudinary Configuration
 cloudinary.config({
    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
